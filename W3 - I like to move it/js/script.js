4ba97daa955017d4ebc121f1419cd88cfa493c14
fioriemay <f_oussel@live.concordia.ca>
2023-09-18 20:36:47 -0400

    setup exercise sept18

---
+++ W3 - I like to move it/js/script.js
@@ -0,0 +1,34 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+    createCanvas(600, 600);
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+    rectMode(CENTER);
+    rect(250,250,100,100);
+}
