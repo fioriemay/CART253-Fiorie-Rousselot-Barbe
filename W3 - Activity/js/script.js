0999c1069dc8ef58ee44db4da0ff5f8985b81583
fioriemay <f_oussel@live.concordia.ca>
2023-09-19 16:43:23 -0400

    finished activity of w3 moving circles

--- W3 - Activity/js/script.js
+++ W3 - Activity/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Activity week 3
+ * Fiorie Rousselot-Barbe
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -15,13 +15,41 @@ function preload() {
 
 }
 
+// variables
+let bg = {
+
+r: 0,
+g: 0,
+b: 0
+
+};
+
+let circle1 = {
+x: 0,
+y: 250,
+size: 100,
+fill: 255,
+alpha: 200
+
+
+};
+
+let circle2 = {
+x: 500,
+y: 250,
+size: 75,
+fill: 255,
+alpha: 200
+
+};
 
 /**
  * Description of setup
 */
 function setup() {
+createCanvas(500, 500);
+    noStroke();
 
-    createCanvas(500, 500);
 }
 
 
@@ -29,9 +57,25 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(0);
-  ellipse(250,250,300,300);
-  ellipse(200,200,50,50);
-  ellipse(300,200,50,50);
-  ellipse(250,300,100,100);
+    background(bg.r, bg.g, bg.b);
+
+    //background colors
+  bg.r = bg.r +1;
+    // left circle
+    circle1.x = circle1.x + 1;
+    circle1.x = constrain(circle1.x, 0, width/2);
+    circle1.size = circle1.size + 0.75;
+    circle1.size = constrain(circle1.size, 0, width);
+
+  fill(circle1.fill, circle1.alpha);
+  ellipse(circle1.x, circle1.y, circle1.size);
+
+  // right circle
+  circle2.x = circle2.x + -1;
+  circle2.x = constrain(circle2.x, width/2, width);
+  circle2.size = circle1.size * 0.75;
+
+
+  fill(circle2.fill, circle2.alpha);
+  ellipse(circle2.x, circle2.y, circle2.size);
 }
