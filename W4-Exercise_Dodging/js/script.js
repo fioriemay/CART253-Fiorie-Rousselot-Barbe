57b33ae42898080a34d78ce758a2e1fc59f15aea
fioriemay <f_oussel@live.concordia.ca>
2023-09-26 19:42:33 -0400

    started W4 exercise

---
+++ W4-Exercise_Dodging/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * Week 4 Exercise
+ * Fiorie Rousselot-Barbe
+ * 
+ * Here is my submission for the exercise, more info
+ * in the readme.md!
+ */
+
+"use strict";
+
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+    createCanvas(500,500);
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+  
+}
