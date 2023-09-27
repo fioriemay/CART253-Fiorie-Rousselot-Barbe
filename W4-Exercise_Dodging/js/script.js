ebe1a7b473f6423babcc59494202e3011c8c564e
fioriemay <f_oussel@live.concordia.ca>
2023-09-26 21:27:17 -0400

    continuing activity
    red circle going from left to right across screen

--- W4-Exercise_Dodging/js/script.js
+++ W4-Exercise_Dodging/js/script.js
@@ -8,6 +8,23 @@
 
 "use strict";
 
+let covid19 = {
+
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  fill: {
+    r: 255,
+    g: 0,
+    b: 0,
+
+  }
+
+}
+
 
 /**
  * Description of preload
@@ -21,7 +38,11 @@ function preload() {
  * Description of setup
 */
 function setup() {
-    createCanvas(500,500);
+    createCanvas(windowWidth, windowHeight);
+
+    covid19.y = random(0, height);
+    covid19.vx = covid19.speed;
+
 }
 
 
@@ -29,5 +50,14 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-  
+  background(0);
+
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
+
+  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+
+  ellipse(covid19.x, covid19.y, covid19.size);
+
+  //CONTINUE AT 15:40
 }
