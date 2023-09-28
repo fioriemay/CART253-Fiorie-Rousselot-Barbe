eed8af5d65372b0ad7f29f0077d84773547c6b44
fioriemay <123784333+fioriemay@users.noreply.github.com>
2023-09-28 14:02:57 -0400

    saved activity separate, duplicated for exercise

---
+++ W4-Exercise_Dodge_Em/js/script.js
@@ -0,0 +1,102 @@
+/**
+ * Week 4 Exercise
+ * Fiorie Rousselot-Barbe
+ * 
+ * Here is my submission for the exercise, more info
+ * in the readme.md!
+ */
+
+"use strict";
+
+let covid19 = {
+
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  fill: {
+    r: 255,
+    g: 0,
+    b: 0,
+
+  }
+
+}
+
+let user = {
+
+x: 250,
+y: 250,
+size: 100,
+fill: 255,
+
+
+}
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    covid19.y = random(0, height);
+    covid19.vx = covid19.speed;
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+  background(0);
+
+  // static background
+  for (let i = 0; i < 275; i++){
+    let x = random(0, width);
+    let y = random (0, height);
+    stroke(255);
+    point (x,y);
+
+  }
+
+  
+
+
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
+
+    if(covid19.x > width){
+
+      covid19.x = 0;
+      covid19.y = random(0, height);
+    }
+
+    user.x = mouseX;
+    user.y = mouseY;
+    
+    let d = dist(user.x, user.y, covid19.x, covid19.y);
+
+    if (d < covid19.size/2 + user.size/2){
+
+      noLoop();
+
+    }
+noStroke();
+  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+  ellipse(covid19.x, covid19.y, covid19.size);
+
+  fill(user.fill);
+  ellipse(user.x, user.y, user.size);
+
+}
