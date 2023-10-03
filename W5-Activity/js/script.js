26e31594fc8eb8b0af159dc78976014b4b473e54
fioriemay <f_oussel@live.concordia.ca>
2023-10-03 19:24:27 -0400

    started week 5 looking for love activity

---
+++ W5-Activity/js/script.js
@@ -0,0 +1,32 @@
+/**
+ * Fiorie Rousselot-Barbe
+ * Exercise of week 5
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
