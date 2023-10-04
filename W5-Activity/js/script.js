cf4c29a2f663f83b9c4d70d5a4b669c070e4d53a
fioriemay <f_oussel@live.concordia.ca>
2023-10-03 21:21:58 -0400

    added main functions
    next step states

--- W5-Activity/js/script.js
+++ W5-Activity/js/script.js
@@ -6,6 +6,7 @@
  * and this description to match your project!
  */
 
+// CONTINUE AT 27:00 !!!!!!!!!!!!!!!!!!!!!!!!!!
 let circle1 = {
 
     x: undefined,
@@ -34,6 +35,11 @@ function setup() {
 
 //project canvas
 createCanvas(500, 500);
+setupCircles();
+
+}
+
+function setupCircles(){
 
 //circles have different x values
 circle1.x = width/3;
@@ -41,7 +47,11 @@ circle2.x= 2*width/3;
 
 //circle movement
 circle1.vx = random(-circle1.speed, circle1.speed);
+circle1.vy = random(-circle1.speed, circle1.speed);
+
 circle2.vx = random(-circle2.speed, circle2.speed);
+circle2.vy = random(-circle2.speed, circle2.speed);
+
 }
 
 
@@ -50,5 +60,53 @@ function draw() {
 
     //black background
     background (0);
+    
+    simulation();
 
+}
+
+function simulation(){
+
+    move();
+   checkOffScreen();
+   checkOverlap();
+   display();
+}
+
+function move(){
+
+    circle1.x = circle1.x + circle1.vx;
+    circle1.y = circle1.y + circle1.vy;
+
+    circle2.x = circle2.x + circle2.vx;
+    circle2.y = circle2.y + circle2.vy;
+}
+
+function checkOffScreen(){
+
+    if(circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height ||circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height){
+
+        // sad ending
+
+    }
+
+}
+
+function checkOverlap(){
+
+    // check if circles overlap
+    let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+    if (d < circle1.size/2 + circle2.size/2){
+
+        // love ending
+
+    }
+
+}
+
+function display(){
+
+    // display circles
+    ellipse(circle1.x, circle1.y, circle1.size);
+    ellipse(circle2.x, circle1.y, circle2.size);
 }
