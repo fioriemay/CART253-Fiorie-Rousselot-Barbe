d80dab8077088d1d1dd8866db63f669eaa0c80b2
fioriemay <f_oussel@live.concordia.ca>
2023-10-04 19:31:49 -0400

    started exercise love actually, edited circle movement

---
+++ W5-Love Actually/js/script.js
@@ -0,0 +1,211 @@
+/**
+ * Fiorie Rousselot-Barbe
+ * Activity of week 5
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+
+//user circle
+let circle1 = {
+
+    x: undefined,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 3,
+
+
+}
+
+let circle2 = {
+
+    x: undefined,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 3,
+    
+
+}
+
+let state = 'title'; // can be : title, simulation, love or sadness.
+
+function setup() {
+
+//project canvas
+createCanvas(500, 500);
+setupCircles();
+
+}
+
+function setupCircles(){
+
+//circles have different x values
+circle1.x = width/3;
+circle2.x= 2*width/3;
+
+//circle movement
+circle1.vx = random(-circle1.speed, circle1.speed);
+circle1.vy = random(-circle1.speed, circle1.speed);
+
+circle2.vx = random(-circle2.speed, circle2.speed);
+circle2.vy = random(-circle2.speed, circle2.speed);
+
+}
+
+
+
+function draw() {
+
+    //black background
+    background (0);
+    
+    if(state === 'title'){
+
+        title();
+
+    }
+
+    else if (state === 'simulation'){
+
+        simulation();
+
+    }
+
+    else if (state === 'love'){
+
+       love();
+
+    }
+
+    else if (state === 'sadness'){
+
+        sadness();
+
+    }
+
+    
+
+}
+
+function title(){
+
+    push();
+    textSize(64);
+    fill(200, 100, 100);
+    textAlign(CENTER, CENTER);
+    text('LOVE?', width/2, height/2);
+    pop();
+
+}
+
+function simulation(){
+
+    move();
+   checkOffScreen();
+   checkOverlap();
+   display();
+
+
+}
+
+function love(){
+
+    push();
+    textSize(64);
+    fill(255, 150, 150);
+    textAlign(CENTER, CENTER);
+    text('LOVE!', width/2, height/2);
+    pop();
+
+}
+
+function sadness(){
+
+    push();
+    textSize(64);
+    fill(150, 150, 255);
+    textAlign(CENTER, CENTER);
+    text(':((', width/2, height/2);
+    pop();
+
+}
+
+function move(){
+
+    /*circle1.x = circle1.x + circle1.vx;
+    circle1.y = circle1.y + circle1.vy;
+*/
+    circle2.x = circle2.x + circle2.vx;
+    circle2.y = circle2.y + circle2.vy;
+
+    if (mouseX > circle1.x){
+  
+        circle1.vx = 2;
+    
+      }
+    
+      else if (mouseX < circle1.x){
+    
+        circle1.vx = -2;
+    
+      }
+      
+      if (mouseY> circle1.y){
+    
+        circle1.vy = 2;
+    
+      }
+    
+      else if (mouseY < circle1.y){
+    
+        circle1.vy = -2;
+    
+      }
+    
+      circle1.x = circle1.x + circle1.vx;
+      circle1.y = circle1.y + circle1.vy;
+}
+
+function checkOffScreen(){
+
+    if(circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height ||circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height){
+
+        state = 'sadness';
+
+    }
+
+}
+
+function checkOverlap(){
+
+    // check if circles overlap
+    let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+    if (d < circle1.size/2 + circle2.size/2){
+
+        state ='love';
+
+    }
+
+}
+
+function display(){
+
+    // display circles
+    ellipse(circle1.x, circle1.y, circle1.size);
+    ellipse(circle2.x, circle1.y, circle2.size);
+}
+
+function mousePressed(){
+
+    if (state === 'title') {
+
+        state = 'simulation';
+
+    }
+
+}
