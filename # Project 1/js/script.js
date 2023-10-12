1b4c47011287a1c080c6470c1cb642855a2578f5
fioriemay <f_oussel@live.concordia.ca>
2023-10-12 14:11:24 -0400

    restarted proj 1, making cat dress-up game instead

--- # Project 1/js/script.js
+++ # Project 1/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Snake Reinvented
+ * Project 1
  * Fiorie Rousselot-Barbe
  * 
  * This is a template. You must fill in the title, author, 
@@ -8,20 +8,11 @@
 
 "use strict";
 
-//size of the grid in my snake
-let gridSize;
+/*
+color palette:
+brown body: 161, 124, 96
+*/
 
-//space between the snake squares
-let space;
-
-//background color
-let bg = {
-
-    r:201,
-    g:181,
-    b:255,
-
-}
 
 function preload() {
 
@@ -30,16 +21,9 @@ function preload() {
 
 
 function setup() {
-    //size of our canvas
-    createCanvas(1000, 1000);
 
-    //making sure the first two parameters are the x/y coordinates and the other two are width & height
-    rectMode(CENTER);
-
-    //amount of squares we can fit in the grid
-    // e.g. if gridSize is set to 5 then the canvas is a 5x5 grid
-    gridSize = 20;
-    space = width / gridSize;
+    //creating a canvas
+    createCanvas(500, 500);
 
 }
 
@@ -47,13 +31,8 @@ function setup() {
 
 function draw() {
 
-    //background depending on the variables in "bg", allowing us to change colors later if needed
-    background(bg.r, bg.g, bg.b);
-
-    noFill();
-    stroke(10, 20, 30);
-    strokeWeight(space);
-
-    //rect (x,y, width, height)
-    rect(width/2, height/2, width, height );
+    //black background
+    background(0);
+    noStroke();
+    ellipse();
 }
