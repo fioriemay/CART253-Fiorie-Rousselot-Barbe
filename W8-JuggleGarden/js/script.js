725c1e425aa1d6af106ce767d73079b5bdad10c9
fioriemay <f_oussel@live.concordia.ca>
2023-10-28 22:12:43 -0400

    Ex:JuggleGarden: Added user-control to plant flowers

--- W8-JuggleGarden/js/script.js
+++ W8-JuggleGarden/js/script.js
@@ -8,6 +8,20 @@
 
 "use strict";
 
+let garden = {
+    // array of flowers (garden.flowers)
+    flowers: [],
+    
+    //amount of flowers we will let exist
+    numFlowers: 0,
+
+    // background color of the canvas / grass
+    grassColor: {
+      r: 120,
+      g: 180,
+      b: 120
+    }
+  };
 
 function preload() {
 
@@ -17,10 +31,55 @@ function preload() {
 
 function setup() {
 
+    createCanvas(600, 600);
+
+    // using a for loop to create our flowers
+    for (let i = 0; i < garden.numFlowers; i++) {
+      
+        //variables regarding the parameters
+      let x = random(0, width);
+      let y = random(0, height);
+      let size = random(50, 80);
+      let stemLength = random(50, 100);
+      let petalColor = {
+        r: random(100, 255),
+        g: random(100, 255),
+        b: random(100, 255)
+      }
+
+      // create flower
+      let flower = new Flower(x, y, size, stemLength, petalColor);
+      //add flower to array
+      garden.flowers.push(flower);
+}
 }
 
 
 
 function draw() {
+ //grass or bg color
+ background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
 
+ // iterate through our array of flowers, then display them
+ for (let i = 0; i < garden.flowers.length; i++) {
+   let flower = garden.flowers[i];
+   flower.display();
+ }
+}
+
+function mousePressed(){
+    let x = random(0, width);
+    let y = random(0, height);
+    let size = random(50, 80);
+    let stemLength = random(50, 100);
+    let petalColor = {
+      r: random(100, 255),
+      g: random(100, 255),
+      b: random(100, 255)
+    }
+
+    let flower = new Flower(x, y, size, stemLength, petalColor);
+      //add flower to array
+      garden.flowers.push(flower);
+    
 }
