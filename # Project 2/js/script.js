c098403827fe2b005963b88338c6913469375ca6
fioriemay <f_oussel@live.concordia.ca>
2023-11-08 23:10:23 -0500

    Proj2: Basic functions added, working on adding ennemies

--- # Project 2/js/script.js
+++ # Project 2/js/script.js
@@ -1,32 +1,83 @@
 /**
- * Title of Project
- * Author Name
+ * Pink Shooter Game (Title TBA)
+ * by Fiorie Rousselot-Barbe
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * 
+ * 
  */
 
 "use strict";
 
-/**
- * Description of preload
-*/
+//background color rgb values
+let bg = {
+
+    r:20,
+    g:17,
+    b:20,
+}
+
+//player variable
+let player = {
+
+    x:20,
+    y:525,
+    w:95,
+    h:95,
+
+}
+
+//array of bullets to group them up more simply when they spawn
+let bullets = [];
+
+
 function preload() {
 
 }
 
 
-/**
- * Description of setup
-*/
+
 function setup() {
+createCanvas(600,600);
+for(let i = 0; i < 10;){
+
+
+}
 
 }
 
 
-/**
- * Description of draw()
-*/
-function draw() {
 
+function draw() {
+//setting the background color
+background(bg.r, bg.g, bg.b);
+
+//used this to locate coordinates easier
+fill(255, 255, 255);
+text(mouseX + ","+ mouseY, 20, 20);
+
+//our player is a circle (for now)
+fill(255,255,255);
+ellipse(mouseX, player.y, player.w, player.h);
+
+//for each bullet in the array "bullets"
+for(let bullet of bullets){
+
+    ellipse(bullet.x, bullet.y, 15);
+    //moving the bullet upwards
+    bullet.y -=6;
+}
+}
+
+// ------- END OF DRAW FUNCTION --------
+
+
+function mousePressed(){
+
+    let bullet = {
+
+        x:mouseX,
+        y:525,
+    
+    }
+bullets.push(bullet);
 }
