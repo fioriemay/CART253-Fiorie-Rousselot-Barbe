3589dfac61c482614445dac31383e18e561822a5
fioriemay <f_oussel@live.concordia.ca>
2023-11-14 19:40:47 -0500

    Ex:Noise: Activity Portion complete

--- W10-MakeSomeNoise/js/script.js
+++ W10-MakeSomeNoise/js/script.js
@@ -30,5 +30,22 @@ function setup() {
 
 
 function draw() {
+    background(0);
 
+    for (let i = 0; i < balls.length; i++) {
+      let ball = balls[i];
+      ball.move();
+      ball.bounce();
+      ball.display();
+    }
 }
+
+function mousePressed() {
+    createBall(mouseX,mouseY);
+  }
+  
+  function createBall(x,y) {
+    let note = random(notes);
+    let ball = new Ball(x,y,note);
+    balls.push(ball);
+  }
